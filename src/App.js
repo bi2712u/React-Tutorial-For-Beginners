245612d7a7e082578bd2ee4b7c53ab9b5e40c3e7
XinYuna <bi2712u@gmail.com>
2019-10-03 00:30:51 +0800

    Final Change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Tweet from './Tweet';
 import './App.css';
+import { isUserWhitespacable } from '@babel/types';
 
 function App(){
+  const [users,setUsers] = useState([
+    {name:"Ed",message:"Hello there"},
+    {name:"John",message:"I am john snow"},
+    {name:"traversy",message:"I am awesome"}
+  ]);
   return(
     <div className="app">
-      <Tweet name="Dev ED" message="This is a random tweet"/>
-      <Tweet name="John Snow" message="I am the true king"/>
-      <Tweet name="Traversy Media" message="700k my dudes"/>
-      <Tweet name="Mosh" message="My new course is available"/>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
